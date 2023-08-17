dff9a6c89096c4203bf9905dfab2d55945694bdf
Rahul P Binu <rahulpbu@gmail.com>
2023-08-17 22:56:46 +0530

    First commit

---
+++ Assignment_1/assignment1.js
@@ -0,0 +1,37 @@
+var myName = 'Rahul';
+console.log(myName);
+console.log('myName', typeof myName);
+
+var mobileNumber = 1234567890;
+console.log(mobileNumber);
+console.log('mobileNumber', typeof mobileNumber);
+
+var myHeight = 168.8;
+console.log(myHeight);
+console.log('myHeight', typeof myHeight);
+
+var trueOrFalse = false;
+console.log(trueOrFalse);
+console.log('trueOrFalse', typeof trueOrFalse);
+
+var cars = ['Polo', 'Civic', 'Swift', 'seltos'];
+console.log(cars);
+console.log('cars', typeof cars);
+
+var person = {First_name :'Sachin', lastname : 'Tendulkar'};
+console.log(person);
+console.log('person', typeof person);
+
+var undefinedVariable;
+console.log(undefinedVariable);
+console.log('undefinedVariable', typeof undefinedVariable);
+
+var nullValue = null;
+console.log(nullValue);
+console.log('nullValue', typeof nullValue);
+
+console.log(myName+ ' ' +mobileNumber+ ' '+myHeight+' '+trueOrFalse+' '+ cars +' '+ person +' '+undefinedVariable+' '+nullValue);
+
+console.log(`${myName} ${mobileNumber} ${myHeight} ${trueOrFalse} ${cars} ${person} ${undefinedVariable} ${nullValue}`);
+
+alert('This is a alert message');

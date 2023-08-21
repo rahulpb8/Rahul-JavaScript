4bbb7147f24ac95e379c502ce66dd5d8951f3c24
Rahul P Binu <rahulpbu@gmail.com>
2023-08-21 10:30:04 +0530

    changes added

--- Assignment_2/assignment2.js
+++ Assignment_2/assignment2.js
@@ -1,18 +1,20 @@
+//1
 let arr = ["1","2","3","4","5","6","7"];
 console.log(arr);
 console.log("\n");
-
+//1.a
 arr.splice(5, 1);
 console.log(arr);
 console.log(`The length of array is : ${arr.length}\n`);
 
-
+//1.b
 arr.forEach(function(item){
     const numberItem = Number(item);
     console.log(`value: ${item} datatype: ${typeof numberItem}`)
 });
 console.log("\n");
 
+//1.c
 //Deleting last 3 values of array
 arr.pop();
 arr.pop();
@@ -24,6 +26,7 @@ console.log(arr);
 arr.unshift("one", "two");
 console.log(arr);
 
+//1.d
 const arr1 = ["1", "2", "3", "4", "5", "6", "7"];
 let concatString = "";
 sum = 0;
@@ -32,14 +35,16 @@ arr1.forEach(function(item){
     concatString = concatString+item;
     sum = sum + Number(item);
 });
-console.log(concatString);
-console.log(sum);
+console.log(`\nConcatinated array is: ${concatString}`);
+console.log(`Sum of array items is: ${sum} \n`);
 
+//1.e
 arrayFiltered = arr1.filter(function(item){
 return item!="3";
 });
 console.log(arrayFiltered);
 
+//1.f
 const arr2 = ["1", "2", "3", "4", "5", "6", "7"];
 
 arr2.forEach(function(item){
@@ -48,21 +53,7 @@ arr2.forEach(function(item){
     }
 });
 
-const cars = ["Porsche", "Polo", "GTR", "Lambhorgini", "Supra"];
-const bikes = ["Continental GT", "MV Agusto", "Triumph Rocket", "Panigale", "SuperDuke"];
-const cars_bike = cars.concat(bikes);
-console.log(cars_bike);
-
-var arr5 = [0,2,3,7,5,6,8];
-
-for(var i=0; i<=arr5.length-1; i++){
-    let result = arr5[i]*i;
-    
-    if(result>40){
-        console.log(`multiplication result of ${arr5[i]}*${i}=${result}.\n`)
-    }
-}
-
+//1.g
 var arr3 = [1, 2, "3", 4, 5, 6,"7"];
 var arr4 = ["1", "2", "3", "4", "5", "6", "7"];
 
@@ -76,6 +67,25 @@ for(var i=0; i<=arr3.length-1;i++){
 }
 console.log("\n");
 
+
+//1.h
+var arr5 = [0,2,3,7,5,6,8];
+
+for(var i=0; i<=arr5.length-1; i++){
+    let result = arr5[i]*i;
+    
+    if(result>40){
+        console.log(`multiplication result of ${arr5[i]}*${i}=${result}.\n`)
+    }
+}
+
+//1.i
+const cars = ["Porsche", "Polo", "GTR", "Lambhorgini", "Supra"];
+const bikes = ["Continental GT", "MV Agusto", "Triumph Rocket", "Panigale", "SuperDuke"];
+const cars_bike = cars.concat(bikes);
+console.log(cars_bike);
+
+//2.
 let n=5;
 let string = "";
 

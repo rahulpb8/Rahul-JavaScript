6a920e87896e502986031d97b6e3c7cedc197619
Rahul P Binu <rahulpbu@gmail.com>
2023-08-25 17:49:44 +0530

    Assignment-5 first commit

---
+++ Assignment_5/javascript/form.js
@@ -0,0 +1,78 @@
+    const form = document.getElementById('form');
+    const nameInput = document.getElementById("name");
+    const phoneInput = document.getElementById("phone");
+    const placeInput = document.getElementById("place");
+    const companyInput = document.getElementById("company");
+    const pinInput = document.getElementById("pincode");
+
+    form.addEventListener('submit', (event) =>{
+        event.preventDefault();
+        checkInputs();
+        const formData = JSON.parse(localStorage.getItem('formData'));
+        if (formData) {
+            nameInput.value = formData.name;
+            phoneInput.value = formData.phone;
+            placeInput.value = formData.place;
+            companyInput.value = formData.company;
+            pinInput.value = formData.pincode;
+        }
+        form.reset();
+    });
+
+    function checkInputs() {
+        const nameInputValue = nameInput.value.trim();
+        const phoneInputValue = phoneInput.value.trim();
+        const placeInputValue = placeInput.value.trim();
+        const companyInputValue = companyInput.value.trim();
+        const pinInputValue = pinInput.value.trim();
+
+        if(nameInputValue === ''){
+            setErrorFor(nameInput, 'Name should not empty');
+            isValid = false
+        }
+        if(phoneInputValue === null || phoneInputValue === '') {
+            setErrorFor(phoneInput, 'Phone number can not be blank');
+            isValid = false
+        }
+        else if(Number.isNaN(phoneInputValue)){
+            setErrorFor(phoneInput, 'Phone number should be an integer');
+            isValid = false
+        }
+        else if(typeof phoneInputValue.length !== 10){
+            setErrorFor(phoneInput, 'Phone number should be 10 integers');
+            isValid = false
+        }
+       if(placeInputValue === '') {
+            setErrorFor(placeInput, 'Place can not be blank, please enter place');
+            isValid = false
+       }
+       if(companyInputValue === '') {
+            setErrorFor(companyInput, 'Company name is required');
+            isValid = false
+       }
+       if(pinInputValue ===''){
+            setErrorFor(pinInput, 'Pincode is required');
+            isValid = false
+       }
+       else if(Number.isNaN(phoneInputValue)){
+            setErrorFor(pinInput, 'Phone number should be an integer');
+            isValid = false
+       }
+
+    }
+
+    function setErrorFor(input, message) {
+        const formControl = input.parentElement;
+        const small = formControl.querySelector('small');
+
+        small.innerText = message;
+
+        formControl.className = 'form-control error';
+    }
+    if (isValid) {
+        const formData = {myname, place, phone, company, pincode,};
+        localStorage.setItem('formData', JSON.stringify(formData));
+        form.reset();
+    }
+
+   
---
+++ Assignment_5/javascript/index.js
@@ -0,0 +1,16 @@
+function showDetails() {
+    const name = 'Rahul P Binu';
+    const phone = 7025503248;
+    const age = 22;
+    const company = 'Coddle Technologies';
+    myDetailsDiv.innerHTML = `Name: ${name}, Phone Number: ${phone}, Age: ${age}, Company: ${company}`;
+    
+}
+const button = document.createElement('button');
+button.textContent = 'show details';
+document.body.appendChild(button);
+
+const myDetailsDiv = document.createElement('div');
+document.body.appendChild(myDetailsDiv);
+
+button.addEventListener('click', showDetails);

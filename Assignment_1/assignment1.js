3cf6cea4a1a32352d126ec258fa14606f21c51d9
rahulpb8 <136778086+rahulpb8@users.noreply.github.com>
2023-09-01 12:22:03 +0530

    Update

--- Assignment_1/assignment1.js
+++ Assignment_1/assignment1.js
@@ -1,3 +1,5 @@
+/*eslint linebreak-style: ["error", "unix"]*/
+//1. Create variables with different data types available in JS and print their data types along with each variable name in the console.
 let myName = 'Rahul';
 console.log(myName);
 console.log('myName', typeof myName);
@@ -28,6 +30,10 @@ console.log('nullValue', typeof nullValue);
 
 console.log(myName+ ' ' +mobileNumber+ ' '+myHeight+' '+trueOrFalse+' '+ cars +' '+undefinedVariable+' '+nullValue);
 
+//2. Create a concatenated string with the above variables using template literals and without template literals. (separate each variable by space in the string)
+
 console.log(`${myName} ${mobileNumber} ${myHeight} ${trueOrFalse} ${cars} ${undefinedVariable} ${nullValue}`);
+
+//3. Write a JS program to show an alert message on the loading of the website.
 
 window.alert('This is a alert message');
